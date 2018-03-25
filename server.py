2993ad9773f62c3f4e98adfc7ba0853c0bf58464
Cristian Duartes <cduartes@debdev.homestation>
2018-03-25 01:28:57 -0300

    server and launcher for windows

---
+++ server.py
@@ -0,0 +1,84 @@
+import socket
+import json
+import os
+
+host = ''
+port = 9100
+main_directory = 'documentRoot'
+
+def Main():
+	#family AF_INET for IPV4 protocol TCP/UDP
+	#connection type SOCK_STREAM for connection-oriented TCP byte stream
+	mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	#socket options: (socket-level, reutilice the socket and value 1 socket is known by this value)
+	mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	#binding port to socket
+	mySocket.bind((host,port))
+	#listen port socket 1
+	mySocket.listen(1)
+	#print server status
+	if(mySocket):
+		if(host == ''):
+			print('Server running on local network port: '+str(port))
+		else:
+			print('Server running on '+host+' port: '+str(port))
+	else:
+		print('Oops! Im having issues setting the socket!')
+
+	#persistent while loop
+	while True:
+		client_sock, client_addr = mySocket.accept()
+		print('Connection from: '+ str(client_addr))
+		request = client_sock.recv(1024).decode()
+		#clean request string
+		request = request.replace('\r\n\r\n','')
+		request = request.replace(':','')
+		request_data = request.split('\r\n')
+
+		#stardarize
+		if(request_data[0].split()[1].endswith('/')):
+			requested_route = main_directory+request_data[0].split()[1]+'index.html'
+		else:
+			requested_route = main_directory+request_data[0].split()[1]
+
+		if(os.path.exists(requested_route)):
+			#correct path 127.0.0.1:9100/app1 (exists) to 127.0.0.1:9100/app1/index.html
+			if not(requested_route.endswith('/')):
+				if not(requested_route.endswith('.html')):
+					requested_route = requested_route+'/index.html'
+			#data dictionary
+			response = {
+			'path': request_data[0].split()[1],
+			'protocol': request_data[0].split()[2],
+			'method': request_data[0].split()[0],
+			}
+
+			#headers dictionary
+			pairs = {}
+			iteration = iter(request_data)
+			next(iteration)
+			for item in iteration:
+				#print(item)
+				item_split = item.split(' ',1)
+				pairs[item_split[0]] = item_split[-1]
+
+			#add headers dictionary
+			response['headers'] = pairs
+			json_data = json.dumps(response)
+
+			#data to send
+			response_text ='X-RequestEcho: '+json_data+'\r\n\r\n'
+			client_sock.sendall('HTTP/1.1 200 OK\r\n'.encode())
+			client_sock.sendall(response_text.encode())
+
+			#deliver html file
+			file = open(requested_route,'r')
+			for line in file:
+				client_sock.sendall(line.encode())
+			file.close()
+		else:
+			client_sock.sendall('HTTP/1.1 404 Not Found\r\n'.encode())
+		client_sock.close()
+
+if __name__ == '__main__':
+	Main()
